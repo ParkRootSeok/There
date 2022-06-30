1224799a5703c14af2588df859d76f68bf47522e
ParkRootSeok <g_e_un@naver.com>
2022-06-30 14:25:40 +0900

    Initial Commit

---
+++ project_There/Home/src/main/java/There/Home/ThereApplication.java
@@ -0,0 +1,13 @@
+package There.Home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThereApplication.class, args);
+	}
+
+}
