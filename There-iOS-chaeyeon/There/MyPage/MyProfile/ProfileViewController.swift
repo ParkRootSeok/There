53b73373eb522725c95cf640de062469f66884a5
mal0070 <mal0070@naver.com>
2022-07-01 23:08:53 +0900

    add mypage

---
+++ There-iOS-chaeyeon/There/MyPage/MyProfile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  There
+//
+//  Created by 이민아 on 2022/07/01.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
