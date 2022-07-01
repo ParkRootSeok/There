0991af5a6a222a8ad032a61b6af2adc5260de40e
s-yeong <syxxng25@gmail.com>
2022-07-01 15:48:02 +0900

    ethan first commit

--- project_There/app/src/main/java/com/there/src/User/UserService.java
+++ project_There/app/src/main/java/com/there/src/User/UserService.java
@@ -1,4 +1,26 @@
 package com.there.src.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final UserDao userDao;
+    private final UserProvider userProvider;
+
+
+    @Autowired
+    public UserService(UserDao userDao, UserProvider userProvider) {
+        this.userDao = userDao;
+        this.userProvider = userProvider;
+
+    }
+
+
 }
