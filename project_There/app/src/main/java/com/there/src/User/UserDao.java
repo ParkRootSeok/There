c96fd4876bd08ead174e2e94243936f7ac2760e5
s-yeong <syxxng25@gmail.com>
2022-07-01 21:33:00 +0900

    ethan commit^^

--- project_There/app/src/main/java/com/there/src/User/UserDao.java
+++ project_There/app/src/main/java/com/there/src/User/UserDao.java
@@ -1,11 +1,13 @@
 package com.there.src.User;
 
 import com.there.src.User.model.GetUserInfoRes;
+import com.there.src.User.model.GetUserPostsRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class UserDao {
@@ -35,6 +37,28 @@ public class UserDao {
                         rs.getString("name")),
                 selectUserInfoParam);
     }
+
+    // 유저 피드 조회 API - 게시물 리스트
+    public List<GetUserPostsRes> selectUserPosts(int userIdx){
+
+        String selectUserPostsQuery ="select p.postIdx as postIdx,\n" +
+                "       p.imgUrl as imgUrl\n" +
+                "from Post as p\n" +
+                "    join User as u on u.userIdx = p.userIdx\n" +
+                "where p.status ='ACTIVE' and u.userIdx=?\n" +
+                "order by p.created_At desc;\n";
+        int selectUserPostsParam = userIdx;
+
+        return this.jdbcTemplate.query(selectUserPostsQuery,
+                (rs, rowNum) -> new GetUserPostsRes(
+                        rs.getInt("postIdx"),
+                        rs.getString("imgUrl")
+                ), selectUserPostsParam);
+
+
+    }
+
+
 
     public static void main(String[] args) {
 
